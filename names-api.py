14c7324a59644382631ba55a96fb78027a340a23
Eric Whyne <ericwhyne@gmail.com>
2017-07-03 10:50:53 -0400

    added male names

--- names-api.py
+++ names-api.py
@@ -5,8 +5,24 @@ import random
 app = Flask(__name__)
 port = 8080
 
-with open('female-names.txt') as f:
+with open('data/female-names.txt') as f:
     female_names = f.read().splitlines()
+with open('data/male-names.txt') as f:
+    male_names = f.read().splitlines()
+
+
+def rand_names(names_list):
+    random_names = []
+    if request.args.get('num'):
+        num = int(request.args.get('num'))
+        if 1 <= num <= len(names_list):
+            random_names = list(map(lambda _: random.choice(names_list), range(num)))
+        else:
+            return "Error: num must be within range 1 to " + str(len(names_list))
+    else:
+        return "Error: please specify a value for num"
+    names = '<br>'.join(random_names)
+    return names
 
 @app.route('/')
 def hello_root():
@@ -14,12 +30,14 @@ def hello_root():
 
 @app.route('/randfemale')
 def rand_female():
-    num = int(request.args.get('num'))
-    random_names = list(map(lambda _: random.choice(female_names), range(num)))
-    names = '<br>'.join(random_names)
-    return 'Random Female Name API ' + str(num) + "<br><br>" + names
+    return rand_names(female_names)
+
+@app.route('/randmale')
+def rand_male():
+    return rand_names(male_names)
 
 if __name__ == '__main__':
     #app.run(host='0.0.0.0',port=port,debug=True)
     app.run(host='0.0.0.0',port=port,threaded=True)
     print "App running..."
+
